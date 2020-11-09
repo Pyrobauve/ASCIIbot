60551afb66c0e38bd4611a1b4c25a6a8607e5aa2
retiolus <65604466+retiolus@users.noreply.github.com>
2020-11-09 20:13:24 +0100

    added typing when command

--- asciibot.py
+++ asciibot.py
@@ -43,66 +43,67 @@ async def help(ctx):
 
 @bot.command()
 async def ascii(ctx):
-    if ctx.message.attachments:
-        files = []
-        for i in ctx.message.attachments:
-            files.append(i)
-            await i.save(f"{path}data/{i.filename}")
-            await ctx.send("Convertion en cours ...", delete_after=1)
-        os.system(f'mv {path}data/{i.filename} {path}data/input.jpg')
+    async with ctx.typing():
+        if ctx.message.attachments:
+            files = []
+            for i in ctx.message.attachments:
+                files.append(i)
+                await i.save(f"{path}data/{i.filename}")
+                await ctx.send("Convertion en cours ...", delete_after=1)
+            os.system(f'mv {path}data/{i.filename} {path}data/input.jpg')
 
-        def main(opt):
-            if opt.mode == "simple":
-               CHAR_LIST = '@%#*+=-:. '
-            else:
-               CHAR_LIST = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-            if opt.background == "white":
-               bg_code = 255
-            else:
-               bg_code = 0
-            font = ImageFont.truetype("{path}fonts/DejaVuSansMono-Bold.ttf", size=10 * opt.scale)
-            num_chars = len(CHAR_LIST)
-            num_cols = opt.num_cols
-            image = cv2.imread(opt.input)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            height, width = image.shape
-            cell_width = width / opt.num_cols
-            cell_height = 2 * cell_width
-            num_rows = int(height / cell_height)
-            if num_cols > width or num_rows > height:
-               print("Too many columns or rows. Use default setting")
-               cell_width = 6
-               cell_height = 12
-               num_cols = int(width / cell_width)
-               num_rows = int(height / cell_height)
-            char_width, char_height = font.getsize("A")
-            out_width = char_width * num_cols
-            out_height = 2 * char_height * num_rows
-            out_image = Image.new("L", (out_width, out_height), bg_code)
-            draw = ImageDraw.Draw(out_image)
-            for i in range(num_rows):
-                line = "".join([CHAR_LIST[min(int(np.mean(image[int(i * cell_height):min(int((i + 1) * cell_height), height),
-                                                          int(j * cell_width):min(int((j + 1) * cell_width),
-                                                                                  width)]) * num_chars / 255), num_chars - 1)]
-                                for j in
-                                range(num_cols)]) + "\n"
-                draw.text((0, i * char_height), line, fill=255 - bg_code, font=font)
+            def main(opt):
+                if opt.mode == "simple":
+                   CHAR_LIST = '@%#*+=-:. '
+                else:
+                   CHAR_LIST = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+                if opt.background == "white":
+                   bg_code = 255
+                else:
+                   bg_code = 0
+                font = ImageFont.truetype("{path}fonts/DejaVuSansMono-Bold.ttf", size=10 * opt.scale)
+                num_chars = len(CHAR_LIST)
+                num_cols = opt.num_cols
+                image = cv2.imread(opt.input)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                height, width = image.shape
+                cell_width = width / opt.num_cols
+                cell_height = 2 * cell_width
+                num_rows = int(height / cell_height)
+                if num_cols > width or num_rows > height:
+                   print("Too many columns or rows. Use default setting")
+                   cell_width = 6
+                   cell_height = 12
+                   num_cols = int(width / cell_width)
+                   num_rows = int(height / cell_height)
+                char_width, char_height = font.getsize("A")
+                out_width = char_width * num_cols
+                out_height = 2 * char_height * num_rows
+                out_image = Image.new("L", (out_width, out_height), bg_code)
+                draw = ImageDraw.Draw(out_image)
+                for i in range(num_rows):
+                    line = "".join([CHAR_LIST[min(int(np.mean(image[int(i * cell_height):min(int((i + 1) * cell_height), height),
+                                                              int(j * cell_width):min(int((j + 1) * cell_width),
+                                                                                      width)]) * num_chars / 255), num_chars - 1)]
+                                    for j in
+                                    range(num_cols)]) + "\n"
+                    draw.text((0, i * char_height), line, fill=255 - bg_code, font=font)
 
-            if opt.background == "white":
-                cropped_image = ImageOps.invert(out_image).getbbox()
-            else:
-                cropped_image = out_image.getbbox()
-            out_image = out_image.crop(cropped_image)
-            out_image.save(opt.output)
+                if opt.background == "white":
+                    cropped_image = ImageOps.invert(out_image).getbbox()
+                else:
+                    cropped_image = out_image.getbbox()
+                out_image = out_image.crop(cropped_image)
+                out_image.save(opt.output)
 
 
-        if __name__ == '__main__':
-          opt = get_args()
-          main(opt)
-        await ctx.message.channel.send(file=discord.File(f"{path}data/output.jpg"))
-        os.remove(f'{path}data/output.jpg')
+            if __name__ == '__main__':
+              opt = get_args()
+              main(opt)
+            await ctx.message.channel.send(file=discord.File(f"{path}data/output.jpg"))
+            os.remove(f'{path}data/output.jpg')
 
-    os.remove(f'{path}data/input.jpg')
-    await ctx.message.delete(delay=None)
+        os.remove(f'{path}data/input.jpg')
+        await ctx.message.delete(delay=None)
 
 bot.run(TOKEN)
